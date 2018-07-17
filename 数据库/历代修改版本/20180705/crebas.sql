c6c4afc4f189a7c7c27640fe4c95ef5ec9762a62
Heath <932872496@qq.com>
2018-07-17 15:48:43 +0800

    2018年7月17日15点48分

--- 数据库/历代修改版本/20180705/crebas.sql
+++ 数据库/历代修改版本/20180705/crebas.sql
@@ -1,10 +1,18 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/7/11 14:55:35                           */
+/* Created on:     2018/7/10 11:39:04                           */
 /*==============================================================*/
 
 
 drop table if exists News;
+
+drop table if exists Newscheck;
+
+drop table if exists Orders;
+
+drop table if exists Pays;
+
+drop table if exists Payslist;
 
 drop table if exists Userscheck;
 
@@ -20,13 +28,65 @@ create table News
    n_flag               national varchar(50) not null,
    n_id                 national varchar(50) not null,
    u_id                 national varchar(50),
+   nc_id                national varchar(50),
    title                national varchar(50) not null,
    n_body               national varchar(50) not null,
-   author               national varchar(50) not null,
+   author               national varchar(50),
    n_date               date not null,
    n_abouts             national varchar(50),
    n_passflag           bool not null,
    primary key (n_id)
+);
+
+/*==============================================================*/
+/* Table: Newscheck                                             */
+/*==============================================================*/
+create table Newscheck
+(
+   nc_id                national varchar(50) not null,
+   nc_commitdate        date not null,
+   nc_enddate           date,
+   nc_flag              bool not null,
+   nc_body              national varchar(50) not null,
+   primary key (nc_id)
+);
+
+/*==============================================================*/
+/* Table: Orders                                                */
+/*==============================================================*/
+create table Orders
+(
+   o_id                 national varchar(50) not null,
+   u_id                 national varchar(50),
+   o_name               national varchar(50) not null,
+   price                numeric(50,0) not null,
+   process              national varchar(50) not null,
+   o_abouts             national varchar(50),
+   primary key (o_id)
+);
+
+/*==============================================================*/
+/* Table: Pays                                                  */
+/*==============================================================*/
+create table Pays
+(
+   u_id                 national varchar(50),
+   watercost            numeric(50,0),
+   thundercost          numeric(50,0),
+   gascost              numeric(50,0),
+   phonecost            numeric(50,0),
+   mouth                national varchar(50) not null
+);
+
+/*==============================================================*/
+/* Table: Payslist                                              */
+/*==============================================================*/
+create table Payslist
+(
+   u_id                 national varchar(50),
+   p_flag               national varchar(50) not null,
+   p_date               date not null,
+   pay                  numeric(50,0) not null
 );
 
 /*==============================================================*/
@@ -70,6 +130,18 @@ create table users
 );
 
 alter table News add constraint FK_Relationship_5 foreign key (u_id)
+      references users (u_id) on delete restrict on update restrict;
+
+alter table News add constraint FK_Relationship_6 foreign key (nc_id)
+      references Newscheck (nc_id) on delete restrict on update restrict;
+
+alter table Orders add constraint FK_Relationship_1 foreign key (u_id)
+      references users (u_id) on delete restrict on update restrict;
+
+alter table Pays add constraint FK_Relationship_3 foreign key (u_id)
+      references users (u_id) on delete restrict on update restrict;
+
+alter table Payslist add constraint FK_Relationship_4 foreign key (u_id)
       references users (u_id) on delete restrict on update restrict;
 
 alter table users add constraint FK_Relationship_2 foreign key (uc_id)
